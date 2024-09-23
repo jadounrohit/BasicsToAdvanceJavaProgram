02a8e7159720ea3c5d335687465bb94f7dd4c093
Rohit Singh Jadoun <117807247+jadounrohit@users.noreply.github.com>
2024-09-23 16:08:15 +0530

    elements changes in SumOfNumbers

--- SumOfNumbers.java
+++ SumOfNumbers.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 public class SumOfNumbers {
 	public static void main(String args[]) {
-		List<Integer> Al=Arrays.asList(10,20,30,40,50);
+		List<Integer> Al=Arrays.asList(10,20,30,100,80,40,50);
 		Optional<Integer> sum=Al.stream().reduce((a,b)->a+b);
 		System.out.println("Sum of numbers :" +sum.get());
 	}
